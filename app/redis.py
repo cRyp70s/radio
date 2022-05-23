3299020742beee5110154c7342fe8c217f086a8e
cRyp70s <matthewwisdom11@gmail.com>
2022-05-23 04:00:23 +0100

    Formatted with black

--- app/redis.py
+++ app/redis.py
@@ -3,24 +3,26 @@ import json
 import redis
 from conf import REDIS_URL
 
+
 class Redis:
     def __init__(self, url: str) -> None:
         """
-            Setup Redis instance
+        Setup Redis instance
         """
         self.instance = redis.from_url(url)
-    
+
     def set(self, key: Union[bytes, str], data, ex: float = None):
         """
-            Serialize and store python object in
-            redis store.
+        Serialize and store python object in
+        redis store.
         """
         serialized_data = json.dumps(data)
         self.instance.set(key, serialized_data, ex=ex)
-    
+
     def get(self, key: Union[bytes, str]) -> Any:
         serialized_data = self.instance.get(key)
         data = json.loads(serialized_data)
         return data
+
 
 redis_backend = Redis(REDIS_URL)
--- app/streamer.py
+++ app/streamer.py
@@ -10,15 +10,22 @@ from conf import RTSP_STREAM, CACHE_FILE_TIME
 from .redis import redis_backend
 
 # Command to stream music to rtsp-simple-server instance with ffmpeg
-COMMAND = ['ffmpeg',
-            '-re',
-            '-stream_loop', '-1',
-            '-i', '-',
-            '-acodec', 'rawaudio',
-            '-c', 'copy',
-            '-vn',
-            '-f', 'rtsp',
-            RTSP_STREAM]
+COMMAND = [
+    "ffmpeg",
+    "-re",
+    "-stream_loop",
+    "-1",
+    "-i",
+    "-",
+    "-acodec",
+    "rawaudio",
+    "-c",
+    "copy",
+    "-vn",
+    "-f",
+    "rtsp",
+    RTSP_STREAM,
+]
 
 logging.basicConfig(level=logging.DEBUG)
 
@@ -26,18 +33,21 @@ logging.basicConfig(level=logging.DEBUG)
 if not os.path.exists("static/"):
     os.mkdir("static/")
 
+
 def get_from_url(playlist, song, song_url):
     """
-        Get song from url or local cache
-        and store in opus format.
+    Get song from url or local cache
+    and store in opus format.
     """
     filename = f"static/{playlist}-{song}"
     filename2 = f"static/{playlist}-{song}.opus"
     logging.debug(f"Retrieving {filename}")
     # Download if file doesnt exists or is stale
-    if not os.path.exists(filename) or \
-        (time.time() - os.path.getmtime(filename)) > CACHE_FILE_TIME:
-        logging.debug(f"Downloading {filename}")       
+    if (
+        not os.path.exists(filename)
+        or (time.time() - os.path.getmtime(filename)) > CACHE_FILE_TIME
+    ):
+        logging.debug(f"Downloading {filename}")
         for _ in range(5):
             # Retry up to five times
             resp = requests.get(song_url)
@@ -45,7 +55,17 @@ def get_from_url(playlist, song, song_url):
                 data = resp.content
                 with open(filename, "wb") as f:
                     f.write(data)
-                com = ['ffmpeg', "-y", "-i", filename, "-c:a", "libopus", "-b:a", "40K", filename2]
+                com = [
+                    "ffmpeg",
+                    "-y",
+                    "-i",
+                    filename,
+                    "-c:a",
+                    "libopus",
+                    "-b:a",
+                    "40K",
+                    filename2,
+                ]
                 sp.check_output(com)
                 break
         else:
@@ -53,11 +73,12 @@ def get_from_url(playlist, song, song_url):
             logging.error(msg)
             resp.raise_for_status()
     with open(filename2, "rb") as f:
-        return f.read(),filename2
+        return f.read(), filename2
+
 
 def run():
     """
-        Run and execute main server loop.
+    Run and execute main server loop.
     """
     PIPE = sp.Popen(COMMAND, stdin=sp.PIPE)
     last_playlist = ""
@@ -70,13 +91,13 @@ def run():
                 # CURRENT_PLAY is a list in the following format
                 # [<playlist>, [(name, song_url, thumbnail_url)..]]
                 playlist, songs = redis_backend.get("CURRENT_PLAY")
-                
+
                 try:
                     # Get ads
                     promotions = redis_backend.get("CURRENT_PROMOTIONS")
                 except Exception as e:
                     logging.exception(e)
-                
+
                 for song_name, url, thumb_url in songs:
                     logging.debug(f"Setting {song_name} as playing.")
                     played += 1
@@ -87,18 +108,29 @@ def run():
                     redis_backend.set("CURRENT_SONG", song_name)
                     PIPE.stdin.write(song_data[0])
                     fp = os.path.abspath(song_data[1])
-                    sl = float(sp.Popen(f'ffprobe -i {fp} -show_entries format=duration -v quiet -of csv="p=0"', shell=True, stdout=sp.PIPE).stdout.read())
-                    sleep = sl*0.95
-                    logging.debug(f"Waiting for {song_name} to finish play {sleep}.")
+                    sl = float(
+                        sp.Popen(
+                            f'ffprobe -i {fp} \
+                              -show_entries format=duration\
+                                -v quiet -of csv="p=0"',
+                            shell=True,
+                            stdout=sp.PIPE,
+                        ).stdout.read()
+                    )
+                    sleep = sl * 0.95
+                    logging.debug(
+                        f"Waiting for {song_name} \
+                                  to finish play {sleep}."
+                    )
                     playlist, songs = redis_backend.get("CURRENT_PLAY")
-                    
+
                     if playlist != last_playlist:
                         # Playlist changed
                         PIPE.kill()
                         PIPE = sp.Popen(COMMAND, stdin=sp.PIPE)
                         break
-                    
-                    if (played%6) == 0:
+
+                    if (played % 6) == 0:
                         # Play a random ad
                         logging.debug("Playing Promotion")
                         try:
@@ -107,12 +139,18 @@ def run():
                             PIPE.stdin.write(song_data[0])
                             redis_backend.set("CURRENT_THUMB", thumb_url)
                             fp = os.path.abspath(song_data[1])
-                            sl = float(sp.Popen(f'ffprobe -i {fp} -show_entries format=duration -v quiet -of csv="p=0"', shell=True, stdout=sp.PIPE).stdout.read())
-                            sleep = sl*0.95
+                            sl = float(
+                                sp.Popen(
+                                    f'ffprobe -i {fp} -show_entries \
+                                        format=duration -v quiet -of csv="p=0"',
+                                    shell=True,
+                                    stdout=sp.PIPE,
+                                ).stdout.read()
+                            )
+                            sleep = sl * 0.95
                             time.sleep(sleep)
                         except Exception as e:
                             logging.exception(e)
                 last_playlist = playlist
         except Exception as e:
             logging.exception(e)
-
